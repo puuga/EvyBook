cd231de920533d44df63b92f892c77ad0f6735b7
Siwawes Wongcharoen <siwaoh@gmail.com>
2016-08-08 16:18:56 +0700

    add lib

---
+++ app/src/main/java/com/appspace/evybook/EvyBookApplication.java
@@ -0,0 +1,11 @@
+package com.appspace.evybook;
+
+import android.app.Application;
+
+/**
+ * Created by siwaweswongcharoen on 8/8/2016 AD.
+ */
+public class EvyBookApplication extends Application {
+    // init Contextor
+    
+}
